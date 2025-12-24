942a168bfb596a622b7ab520dfc3b13b38da434c
yejomy <yejomy@gmail.com>
2025-12-24 16:19:33 +0900

    Add files via upload

---
+++ 02_cssfunction/script/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    // $("선택자").css({"속성":"값})
+    $("h1").css({"color":"red"})
+    $(".date").css({"color":"#ffffff", "background":"#81007f", 
+        "font-size":80, // 숫자데이터는 ""안써도됨
+        "weight":"1460px", "height":"120px"})
+    })
+    $(".para").css({"border":"10px solid blue"})
+    $("ul li").css({"border":"1px solid red"})
+    $("ul li:first-child").css({"font-size":"24px","font-weight":200})
+    $("ul li:last-child").css({"font-size":"48px","font-weight":500})
+    $("a").css({"border":"5px solid blue", "padding":"8px 10px"})
