3d83c34243e0b921f0b35605f247ce2f8ca49c89
yejomy <yejomy@gmail.com>
2025-12-24 15:34:33 +0900

    Add files via upload

---
+++ 01_jquerybinding/script/main.js
@@ -0,0 +1,12 @@
+$(function(){
+// 코드작성
+// 버튼을 click 하면 div box의 내부색을 바꾼다 
+
+// 버튼을 클릭하면
+
+$('button').click(function(){
+    // div박스의 내부색을 red로 변경
+    $("div").css({"background":"red"})
+})
+
+})
