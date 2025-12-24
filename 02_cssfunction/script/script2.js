2014f153daf254891a887553427b1f7efcb744c6
yejomy <yejomy@gmail.com>
2025-12-24 16:51:16 +0900

    Add files via upload

---
+++ 02_cssfunction/script/script2.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("li").css({"list-style":"none"});
+
+    $("div").css({"width":"100%"});
+
+    $("ul").css({
+        "display":"flex","gap":"20px", "justify-content":"center",
+        "margin":"0 auto"
+    });
+
+    $("ul li").css({"width":"205px", "height":"205px"});
+
+    $(".box1").css({"background":"#fe0000", "border":"1px solid #000"});
+    $(".box2").css({"background":"#fea500", "border":"1px solid #000"});
+    $(".box3").css({"background":"#ffff00", "border":"1px solid #000"});
+    $(".box4").css({"background":"#008001", "border":"1px solid #000"});
+});
