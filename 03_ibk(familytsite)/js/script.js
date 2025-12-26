92e51377f081f2922bee02742ad1aadb323d583f
yejomy <yejomy@gmail.com>
2025-12-26 17:27:56 +0900

    Add files via upload

--- 03_ibk(familytsite)/js/script.js
+++ 03_ibk(familytsite)/js/script.js
@@ -6,11 +6,14 @@ $(function(){
     // 패밀리1 버튼
     $(".family button").click(function(){
     $(".family .family_list").toggle();
+    $(".family2 .family_list").hide();
     });
 
     // 패밀리2 버튼
     $(".family2 button").click(function(){
     $(".family2 .family_list").toggle();
+    $(".family .family_list").hide();
     });
 
+    //한쪽 박스 클릭 block
 });
