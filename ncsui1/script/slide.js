b09dfb70e1b5c992f9c772e850eeeaf36d44a830
yejomy <yejomy@gmail.com>
2025-12-29 16:59:40 +0900

    Add files via upload

---
+++ ncsui1/script/slide.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    loop: false, // 반복 ❌
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // ✅ 마지막 슬라이드 도달 시 autoplay 중지
+    on: {
+      reachEnd: function () {
+        this.autoplay.stop();
+      },
+    },
+  });
+
+  document.querySelector('.start-button').addEventListener('click', () => {
+    window.location.href = 'https://www.naver.com';
+  });
+});
