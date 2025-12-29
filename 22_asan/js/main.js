e3ccc52b403c468b527c8acb0b3834568a5b37a6
yejomy <yejomy@gmail.com>
2025-12-29 12:44:32 +0900

    Add files via upload

---
+++ 22_asan/js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+   $('.slickslide').slick({
+    // 자동재생
+    autoplay:true,
+    dots:true,
+    // 자동재생시간
+    autoplaySpeed:2000,
+    // 좌우버튼
+    arrows:false,
+    // fade효과
+    // fade:true,
+    //슬라이드 효과 상하
+    // vertical:true,
+    // 슬라이드 효과 반복않고 1번만 실행
+    // infinite:false,
+    // pauseOnHover:true,
+
+    // sliesToScroll:1
+  });
+});
